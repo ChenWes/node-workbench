74f37bf110d2257761caf549ab514bf699e3b9cb
Wes Chen /GET/IT <ChenWes@esquel.com>
2017-05-09 14:27:20 +0800

    change file for project

--- node-workbench/app/controllers/home.js
+++ node-workbench/app/controllers/home.js
@@ -14,7 +14,25 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/main', function (req, res, next) {
-  res.render('main', {
+  res.render('manage/main', {
+    title: 'Nodejs Workbench'
+  });
+});
+
+router.get('/home', function (req, res, next) {
+  res.render('manage/home', {
+    title: 'Nodejs Workbench'
+  });
+});
+
+router.get('/form', function (req, res, next) {
+  res.render('manage/form', {
+    title: 'Nodejs Workbench'
+  });
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('home/login', {
     title: 'Nodejs Workbench'
   });
 });
