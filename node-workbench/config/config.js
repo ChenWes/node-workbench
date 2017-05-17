71b9ed25f5da7ac7710bac9653c2ec6e70b4a8d0
Wes Chen /GET/IT <ChenWes@esquel.com>
2017-05-17 23:05:45 +0800

    change bower setting
    * install font-awesome/bootstrap/jquery from bower
    * change layout css and js src

--- node-workbench/config/config.js
+++ node-workbench/config/config.js
@@ -1,7 +1,7 @@
 var path = require('path'),
   rootPath = path.normalize(__dirname + '/..'),
   env = process.env.NODE_ENV || 'development',
-  projectPort = 3002;
+  projectPort = 3000;
 
 var config = {
   development: {
